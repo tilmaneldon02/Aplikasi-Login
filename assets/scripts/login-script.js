341a41630ff2bd53683e10ea5faeabbc73e95152
TilmanEldon <126577609+tilmaneldon02@users.noreply.github.com>
2024-02-18 23:40:55 +0700

    Update login-script.js
    Mengganti komentar sesuai dengan format JSDoc

--- assets/scripts/login-script.js
+++ assets/scripts/login-script.js
@@ -5,21 +5,37 @@
  * Gunakan fungsi showPopUp() untuk menampilkan pop up error
  */
 
+/* Comment : Membuat Varialbel untuk setiap element view. */
 const loginFormElement = document.querySelector('#loginForm');
 const inputEmailElement = document.querySelector('#inputEmail');
 const inputPasswordElement = document.querySelector('#inputPassword');
 
+/*Mmebuat variable untuk menyimpan informasi email dan password */
 const expectedEmail = 'admin@dicoding.com';
 const expectedPassword = 'superpassword';
 
+/*menambahkan aksi klik pada buttom */
 loginFormElement.addEventListener('submit', function(event) {
-  event.preventDefault();
+    event.preventDefault();
+    // TODO 1 : Mendapatkan input email dan password pengguna dari form.
+  
+    /*mendapatkan nilai dari masing2 input ketika tombol ditekan */
+    const email = inputEmailElement.value;
+    const password = inputPasswordElement.value;
 
-  const email = inputEmailElement.value;
-  const password = inputPasswordElement.value;
-  if (email == expectedEmail && password == expectedPassword) {
-    goToHome();
-  } else {
-    showPopUp();
-  }
+    /* TODO 2 : Buat Logika perbandingan dengan kondisi:
+       Jika variabel email identik dengan expectedEmail dan password identik dengan expectedPassword, panggil fungsi goToHome().
+       Jika tidak, maka panggil fungsi showPopUp().
+     */
+
+       /*Memastikan bahwa nilai email dan password sesuai dengan nilai yang tersimpan. */
+    if (email == expectedEmail && password == expectedPassword) {
+      /* Comment : Jika sesuai maka program akan berpindah ke halaman home. */
+      goToHome();
+    } else {
+
+      /* Comment : Namun jika tidak sesuai maka akan menampilkan informasi bahwa input salah. */
+      showPopUp();
+    }
+
 });
